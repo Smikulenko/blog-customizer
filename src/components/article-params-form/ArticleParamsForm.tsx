1920c1546b2edf311cea5bfa0953d5edb25ebbcb
sofya Mikulenko <nephilim45@yandex.ru>
2025-05-01 17:37:58 +1000

    first-rewiew

--- src/components/article-params-form/ArticleParamsForm.tsx
+++ src/components/article-params-form/ArticleParamsForm.tsx
@@ -1,14 +1,116 @@
+import { useState } from 'react';
+import clsx from 'clsx';
+
+import { Select } from 'src/ui/select';
+import { Separator } from 'src/ui/separator';
+import { Text } from 'src/ui/text';
 import { ArrowButton } from 'src/ui/arrow-button';
 import { Button } from 'src/ui/button';
+import {
+	fontFamilyOptions,
+	fontSizeOptions,
+	defaultArticleState,
+	backgroundColors,
+	fontColors,
+	contentWidthArr,
+	ArticleStateType,
+} from 'src/constants/articleProps';
 
 import styles from './ArticleParamsForm.module.scss';
+import { RadioGroup } from 'src/ui/radio-group';
 
-export const ArticleParamsForm = () => {
+type ArticleParamsFormProps = {
+	articleParams: (options: ArticleStateType) => void;
+};
+
+export const ArticleParamsForm = ({
+	articleParams,
+}: ArticleParamsFormProps) => {
+	const [open, setOpen] = useState(false);
+	const [fontFamily, setFontFamily] = useState(
+		defaultArticleState.fontFamilyOption
+	);
+	const [fontSize, setFontSize] = useState(defaultArticleState.fontSizeOption);
+	const [fontColor, setFontColor] = useState(defaultArticleState.fontColor);
+	const [background, setBackground] = useState(
+		defaultArticleState.backgroundColor
+	);
+	const [WidthArr, setWidthArr] = useState(defaultArticleState.contentWidth);
+	const toggleArrowButton = () => {
+		setOpen(!open);
+	};
+	const formStylle = clsx(styles.container, { [styles.container_open]: open });
+
+	const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+		e.preventDefault();
+		articleParams({
+			fontFamilyOption: fontFamily,
+			fontSizeOption: fontSize,
+			fontColor: fontColor,
+			backgroundColor: background,
+			contentWidth: WidthArr,
+		});
+	};
+	const handleReset = (e: React.FormEvent<HTMLFormElement>) => {
+		e.preventDefault();
+		articleParams({
+			fontFamilyOption: defaultArticleState.fontFamilyOption,
+			fontSizeOption: defaultArticleState.fontSizeOption,
+			fontColor: defaultArticleState.fontColor,
+			backgroundColor: defaultArticleState.backgroundColor,
+			contentWidth: defaultArticleState.contentWidth,
+		});
+	};
 	return (
 		<>
-			<ArrowButton isOpen={false} onClick={() => {}} />
-			<aside className={styles.container}>
-				<form className={styles.form}>
+			<ArrowButton isOpen={open} onClick={toggleArrowButton} />
+			<aside className={formStylle}>
+				<form
+					className={styles.form}
+					onSubmit={handleSubmit}
+					onReset={handleReset}>
+					<Text size={31} weight={800} uppercase>
+						Задайте параметры
+					</Text>
+
+					<Select
+						selected={fontFamily}
+						onChange={setFontFamily}
+						options={fontFamilyOptions}
+						title='Шрифт'
+					/>
+
+					<RadioGroup
+						name='radio'
+						onChange={setFontSize}
+						selected={fontSize}
+						options={fontSizeOptions}
+						title='размер шрифта'
+					/>
+
+					<Select
+						onChange={setFontColor}
+						selected={fontColor}
+						options={fontColors}
+						title='цвет шрифта'
+					/>
+
+					<Separator />
+
+					<Select
+						selected={background}
+						onChange={setBackground}
+						options={backgroundColors}
+						title='цвет фона'
+					/>
+
+					<Select
+						selected={WidthArr}
+						onChange={setWidthArr}
+						options={contentWidthArr}
+						title='ширина контента'
+					/>
+
 					<div className={styles.bottomContainer}>
 						<Button title='Сбросить' htmlType='reset' type='clear' />
 						<Button title='Применить' htmlType='submit' type='apply' />
